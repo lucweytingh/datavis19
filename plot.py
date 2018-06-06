fa97e7c524d23a50c691e797675b5caee0a33052
Jeroen Jagt <jeroenpjagt@gmail.com>
2018-06-06 11:27:04 +0200

    Added run.py

--- plot.py
+++ plot.py
@@ -1,23 +1,18 @@
 from init import *
 
-def plot_data(filter, columns, axes = None, blacklist = []):
-  results = pd_data.filter(filter).dict_from_columns(columns, axes, blacklist)
+def plot_data(data, columns, axes = None, blacklist = []):
+  results = data.dict_from_columns(columns, axes, blacklist)
 
   for market, items in results.items():
     plt.clf()
     for item, dic in items.items():
-      print(item)
-      print(dic)
       plt.title(f"{market}")
       plt.plot(dic["date"], dic["price"], label=item)
     plt.legend(loc='upper right')
     plt.show()
 
-def plot_by_item(filter):
-  plot_data(filter, ['item_name', 'market_name'])
+def plot_by_item(data, blacklist = []):
+  plot_data(data, ['item_name', 'market_name'], None, blacklist)
 
-def plot_by_market(filter):
-  plot_data(filter, ['market_name', 'item_name'])
-
-
-plot_by_item({ 'country_name': 'Niger' })
+def plot_by_market(data, blacklist = []):
+  plot_data(data, ['market_name', 'item_name'], None, blacklist)
