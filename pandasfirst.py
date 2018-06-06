b7b87b582e6fbc5111c88a65319bbee57d0f9419
Pim Meerdink <pimmeerdink@hotmail.com>
2018-06-06 11:26:41 +0200

    pandafirst small edits

--- pandasfirst.py
+++ pandasfirst.py
@@ -1,4 +1,12 @@
 from init import *
 
 
-print(ph.filter(ph.filter(pd_data, 'country_id', 1),'item_id',145)['price'])
+# print(ph.filter(ph.filter(pd_data, 'country_id', 1),'item_id',145)['price'])
+
+
+# pd_data1 = pd_data.var()
+
+
+# print(pd_data1((pd_data1.country_id == 1) & (pd_data1.item_id == 145)))
+
+print(ph.filter(pd_data, {'country_id':1, 'item_id':284}))
