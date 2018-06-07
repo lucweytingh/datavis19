60f83241b4c230c3270326b0ae19b868b3e4e638
Pim Meerdink <pimmeerdink@hotmail.com>
2018-06-07 11:18:26 +0200

    find prices af

--- exchange_finder.py
+++ exchange_finder.py
@@ -1,12 +1,18 @@
 from init import *
-
 df = pd.read_csv('../exchange.csv')
+
+dates = ["2017-03","2017-04","2017-05","2017-06"]
+currency = "AFN"
+stattype = "Average of observations through period"
+
+
+
 
 def currency_dictionary_maker(currency,stattype,dates):
     dict = {}
     months = df.filter({'CURRENCY': currency, "Collection": stattype, 'Frequency': 'Monthly'})
     for date in dates:
-        dict[date] = months[date].unique()[0]
+        dict[date] = bmonths[date].unique()[0]
     print(dict)
 
-
+currency_dictionary_maker(currency,stattype,dates)
