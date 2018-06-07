bb6b2c3d802d019bdddf786ab9138f3853cbe92d
Luc Weytingh <lucweytingh321@gmail.com>
2018-06-07 16:49:07 +0200

    regions add to csv file

---
+++ add_regions.py
@@ -0,0 +1,48 @@
+from init import *
+
+dic = {'Afghanistan': 1, 'Algeria': 2, 'Armenia': 3, 'Azerbaijan': 3, 'Bangladesh': 4,
+       'Benin': 5, 'Bhutan': 4, 'Bolivia': 6, 'Burkina Faso': 5, 'Burundi': 5, 'Cambodia': 7,
+       'Cameroon': 5, 'Cape Verde': 5, 'Central African Republic': 5, 'Chad': 5,
+       'Colombia': 6, 'Congo': 5, 'Costa Rica': 6, "Cote d'Ivoire": 5,
+       'Democratic Republic of the Congo': 5, 'Djibouti': 2, 'El Salvador': 6,
+       'Ethiopia': 5, 'Gambia': 5, 'Georgia':3, 'Ghana':5, 'Guatemala':6,
+       'Guinea-Bissau':5, 'Guinea':5, 'Haiti':6, 'Honduras':6, 'India':4,
+       'Indonesia':7, 'Iran  (Islamic Republic of)':2, 'Iraq':2, 'Jordan':2,
+       'Kenya':5, 'Kyrgyzstan':3, "Lao People's Democratic Republic":7,
+       'Lebanon':2, 'Lesotho':5, 'Liberia':5, 'Madagascar':5, 'Malawi':5, 'Mali':5,
+       'Mauritania':5, 'Mozambique':5, 'Myanmar':7, 'Nepal':1, 'Niger':5, 'Nigeria':5,
+       'Pakistan':1, 'Panama':6, 'Peru':6, 'Philippines':7, 'Rwanda':5, 'Senegal':5,
+       'Sri Lanka': 1, 'Swaziland':5, 'Syrian Arab Republic':2, 'Tajikistan':3,
+       'Timor-Leste':7, 'Turkey':3, 'Uganda':5, 'Ukraine':3,
+       'United Republic of Tanzania':5, 'Yemen':2, 'Zambia':5, 'Zimbabwe':5,
+       'State of Palestine':2, 'Sudan':5, 'Egypt':2, 'South Sudan':5}
+
+lis = ['Afghanistan', 'Algeria', 'Armenia', 'Azerbaijan', 'Bangladesh',
+       'Benin', 'Bhutan', 'Bolivia', 'Burkina Faso', 'Burundi', 'Cambodia',
+       'Cameroon', 'Cape Verde', 'Central African Republic', 'Chad',
+       'Colombia', 'Congo', 'Costa Rica', "Cote d'Ivoire",
+       'Democratic Republic of the Congo', 'Djibouti', 'El Salvador',
+       'Ethiopia', 'Gambia', 'Georgia', 'Ghana', 'Guatemala',
+       'Guinea-Bissau', 'Guinea', 'Haiti', 'Honduras', 'India',
+       'Indonesia', 'Iran  (Islamic Republic of)', 'Iraq', 'Jordan',
+       'Kenya', 'Kyrgyzstan', "Lao People's Democratic Republic",
+       'Lebanon', 'Lesotho', 'Liberia', 'Madagascar', 'Malawi', 'Mali',
+       'Mauritania', 'Mozambique', 'Myanmar', 'Nepal', 'Niger', 'Nigeria',
+       'Pakistan', 'Panama', 'Peru', 'Philippines', 'Rwanda', 'Senegal',
+       'Sri Lanka', 'Swaziland', 'Syrian Arab Republic', 'Tajikistan',
+       'Timor-Leste', 'Turkey', 'Uganda', 'Ukraine',
+       'United Republic of Tanzania', 'Yemen', 'Zambia', 'Zimbabwe',
+       'State of Palestine', 'Sudan', 'Egypt', 'South Sudan']
+
+total = []
+for country in lis:
+    amount = pd_data.filter({'country_name':country})['country_id'].count()
+    add = []
+    for i in range(amount):
+        add.append(dic[country])     
+    total = total + add
+
+pd_data['region_id'] = total
+
+pd_data.to_csv('region_data.csv', encoding='utf-8', index=False)
+
--- init/__init__.py
+++ init/__init__.py
@@ -10,8 +10,9 @@ pd.core.frame.DataFrame.filter = ph.filter
 pd.core.frame.DataFrame.get_list = ph.get_list
 pd.core.frame.DataFrame.dict_from_columns = ph.dict_from_columns
 
+# , sep=';', dtype={'region_id': int,'region_name':str, 'country_id': int, 'country_name':str ,'city_id':int,'city_name':str,'market_id':int,'market_name':str,'item_id':int,'item_name':str,'cur_id':int,'cur_name':str,'pt_id':int,'pt_name':str,'unit_id':int,'unit_name':str,'month':int,'year':int,'price':float,'source':str})
+def init_data(filename = "data.csv"):
+  return pd.read_csv(filename, encoding='latin-1')
 
-def init_data(filename = "region_data.csv"):
-  return pd.read_csv(filename, encoding='latin-1', sep=';', dtype={'region_id': int,'region_name':str, 'country_id': int, 'country_name':str ,'city_id':int,'city_name':str,'market_id':int,'market_name':str,'item_id':int,'item_name':str,'cur_id':int,'cur_name':str,'pt_id':int,'pt_name':str,'unit_id':int,'unit_name':str,'month':int,'year':int,'price':float,'source':str})
 
 pd_data = init_data()
