cfbed03ced4ab7586178fdc836f829324588e013
Luc Weytingh <lucweytingh321@gmail.com>
2018-06-19 13:47:49 +0200

    correction on compare_regions

--- compare_regions.py
+++ compare_regions.py
@@ -2,8 +2,8 @@ from init import *
 
 
 def main():
-    data, item = compare(5, 'Wheat')
-    plot1 = figure(x_axis_type="datetime", title="Average food price of " + item)
+    data, item = compare(6)
+    plot1 = figure(x_axis_type="datetime", title="Average food price of " + item.lower())
     legend = []
     for name in data:
         date_values = sort_dates(data[name])
@@ -30,6 +30,7 @@ def add_zero(x):
     else:
         return str(x)
 
+# split the date and values in two separate lists
 def split_date_and_values(both):
     return [x[0] for x in both], [x[1] for x in both]
 
@@ -46,11 +47,15 @@ def sort_dates(dates):
         del(dates[0])
         while dates != []:
             for i in range(len(result)):
-                if dates[0][0][1] <= result[i][0][1]:
+                if dates[0][0][1] < result[i][0][1]:
+                    result.insert(i, dates[0])
+                    del(dates[0])
+                    break
+                elif dates[0][0][1] == result[i][0][1]:
                     if dates[0][0][0] < result[i][0][0]:
                         result.insert(i, dates[0])
                         del(dates[0])
-                        break
+                        break 
                 if i == len(result) - 1:
                     result.append(dates[0])
                     del(dates[0])
@@ -103,4 +108,8 @@ if __name__ == "__main__":
     from bokeh.layouts import gridplot
     from bokeh.plotting import figure, show, output_file
     from bokeh.models import Legend
+    # test_dates = [((1, 2006), 0.2768810516701759), ((2, 2006), 0.28593047558787704), ((3, 2006), 0.28802077470255943), ((4, 2006), 0.28738613596635715), ((5, 2006), 0.2860767156720411), ((6, 2006), 0.29261475828304473), ((7, 2006), 0.27963102968064624), ((11, 2006), 0.32396098841041737), ((1, 2004), 0.2527255874820858), ((2, 2004), 0.2596318750083353), ((3, 2004), 0.2576683009971368), ((4, 2004), 0.2674213554900618), ((5, 2004), 0.25713157618574906), ((6, 2004), 0.25772099841343993), ((7, 2004), 0.2580106042676619), ((8, 2004), 0.2551941803997591), ((9, 2004), 0.2589134990052946), ((10, 2004), 0.2597369740466546), ((11, 2004), 0.28608626565652484), ((12, 2004), 0.27836322846993933), ((1, 2005), 0.26436998595299055)]
+    # test_dates = sort_dates(test_dates)
+    # dates, values = split_date_and_values(test_dates)
+    # print(dates)
     main()
