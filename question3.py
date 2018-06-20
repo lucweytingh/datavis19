3ae06a9ef1df7bff40d4f408e12f27bd65b38626
Pim Meerdink <pimmeerdink@hotmail.com>
2018-06-20 11:54:18 +0200

    undernourishment plot and calc

--- question3.py
+++ question3.py
@@ -2,6 +2,7 @@ from init import *
 from bokeh.layouts import gridplot
 from bokeh.plotting import figure, show, output_file
 from bokeh.models import Legend
+from bokeh.models import Range1d
 
 
 def food_data(country, item):
@@ -35,6 +36,16 @@ def plot(plot, legend_names):
     plot.grid.grid_line_alpha=0.3
     plot.xaxis.axis_label = 'Date'
     plot.yaxis.axis_label = 'Price (usd)'
+    legend = Legend(items=legend_names, location=(0, -30))
+    plot.add_layout(legend, 'right')
+    show(gridplot([[plot]], plot_width=1000, plot_height=600))  # open a browser
+
+
+def plotundernourishment(plot, legend_names):
+    plot.y_range = Range1d(0, 100)
+    plot.grid.grid_line_alpha=0.3
+    plot.xaxis.axis_label = 'Date'
+    plot.yaxis.axis_label = 'Undernourishment index'
     legend = Legend(items=legend_names, location=(0, -30))
     plot.add_layout(legend, 'right')
     show(gridplot([[plot]], plot_width=1000, plot_height=600))  # open a browser
@@ -88,41 +99,80 @@ def listconverter(dict1,index):
 def split_date_and_values(both):
     return [x[0] for x in both], [x[1] for x in both]
 
-def plotter(country, select):
-    if select == 'under':
-        index = 1     
-    elif select == 'price':
-        index = 0
-    else:
-        return 1
+def plotter(country):
     products = pd_data.filter({'country_name':country})['item_name'].unique()
-    plot1 = figure(x_axis_type="datetime", title=select)
+    plot1 = figure(x_axis_type="datetime", title= 'Food Price')
     legend = []
     for item in products:
-        itemdata = listconverter(nodata_filter(food_data(country,item)),index)
-        
+        itemdata = listconverter(nodata_filter(food_data(country,item)),0)
         sorteddates = sort_dates(itemdata)
         print(item)
-
         dates , data = split_date_and_values(sorteddates)
         legend.append((item,[add_to_plot(plot1,item,dates,data)]))
     plot(plot1,legend)
 
-#plotter('Lebanon','under')
+def checkEqual2(iterator):
+   return len(set(iterator)) <= 1
+
 
 def coeff(country):
     products = pd_data.filter({'country_name':country})['item_name'].unique()
+    coeffs = {}
     for item in products:
         itemdata = listconverter(nodata_filter(food_data(country,item)),0)
-        sorteddates = listconverter(nodata_filter(food_data(country,item)),1)
+        noudata = listconverter(nodata_filter(food_data(country,item)),1)
         dates, itemdata = split_date_and_values(itemdata)
-        dates, sorteddates = split_date_and_values(sorteddates)
+        dates, noudata  = split_date_and_values(noudata)
         itemdata = list(map(float,itemdata))
-        sorteddates = list(map(float,sorteddates))
-        print(item)
-        print(np.corrcoef(itemdata,sorteddates)[0][1])
-     
-coeff('Afghanistan')
-plotter('Afghanistan','price')
+        noudata = list(map(float,noudata))
+        if checkEqual2(noudata):
+            coeffs[item] = 0
+        else:
+            coeffs[item] = np.corrcoef(itemdata,noudata)[0][1]
+    return coeffs
+    
+
+def undernourishmentlist(country):
+    data = pd_data.filter({'country_name' : country})
+    result = []
+    for index, row in data.iterrows():
+        if row['undernourishment'] != 'No Data':
+            result.append(((row['month'],row["year"]),row['undernourishment']))
+    return list(set(result))
+
+def undernourishmentplotter(country):
+    plot1 = figure(x_axis_type="datetime", title=country + 'undernourishment')
+    legend = []
+    data = undernourishmentlist(country)
+    sorteddata = sort_dates(data)
+    dates , data = split_date_and_values(sorteddata)
+    legend.append(('name',[add_to_plot(plot1,'name',dates,data)]))
+    plotundernourishment(plot1,legend)
+
+def produce_result(country):
+    coeff(country)
+    plotter(country)
+    undernourishmentplotter(country)
+
+
+#produce_result('Ghana')
+
+
+def biggestcorrelator():
+    countries = pd_data['country_name'].unique()
+    res = []
+    for country in countries:
+        print(country)
+        countrydict = coeff(country)
+        for key, value in countrydict.items():
+            res.append(((country,key),value))
+
+    sortedlist = sorted(res,key=lambda x: x[1], reverse=True)
+    ten = sortedlist[:10]
+    for i in range(10):
+        obj = ten[i]
+        print('Correlation ' + str(i) + ' is ' + str(obj[0][1]) + ' in ' + str(obj[0][0]) + ' with R ' + str(obj[1]))
+
+print(biggestcorrelator())
 
 
